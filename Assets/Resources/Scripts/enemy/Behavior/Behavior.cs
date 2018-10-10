b7793a443dcd6cafda93d2eea537ce753197a2fd
ORECROS\Drew <orecros@gmail.com>
2018-10-09 20:38:35 -0500

    added enemy "behaviors", which causes the enemy to perform an action. base enemy only accept one behavior, but the plan is to give its descendents multiple behaviors, and it uses a state machine to decide which one to perform

---
+++ Assets/Resources/Scripts/enemy/Behavior/Behavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Behavior : MonoBehaviour {
+    public abstract void PerformAction();
+
+    
+    //saving some components here for convenience functions
+    protected Enemy enemy;
+    protected BoxCollider2D col;
+
+    public virtual void Start() {
+        enemy = GetComponent<Enemy>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // useful functions for behaviors
+    public bool IsGrounded() {
+        //do an overlapCircle near our feet to see if there's ground there
+        return CheckDirection(Vector2.down);
+    }
+    public bool CheckDirection(Vector2 direction) {
+
+        //if overlapCircle returns anything BUT null then we've found something
+        return null != Physics2D.OverlapCircle(transform.position + (Vector3)(direction * col.size / 2), 0.1f, LAYER.SOLIDS);
+    }
+
+}
---
+++ Assets/Resources/Scripts/enemy/Behavior/Behavior_Pace.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior_Pace : Behavior {
+
+    public float WalkSpeed = 5;
+    /// <summary>
+    /// if true, the enemy will turn when it reaches a ledge. if false, it will fall off the ledge.
+    /// </summary>
+    public bool DoTurnAtLedges = true;
+    /// <summary>
+    /// Time in seconds the enemy will be still when changing direction
+    /// </summary>
+    public float TurnPauseTime = 0.25f;
+
+    bool isPaused = false;
+    float nextTurnTime;
+
+    public override void PerformAction() {
+        if(isPaused) {
+            //if we're supposed to wait to turn
+            if (Time.time > nextTurnTime)
+                //if we're done waiting turn
+                ResolveChangeDirection();
+            else {
+                //keep waiting
+                enemy.DesiredVelocity = Vector2.zero;
+                return;
+            }
+        }
+
+        if (CheckDirection(enemy.FacingRight ? Vector2.right : Vector2.left)) {
+            //if there's a wall in front of us in the direction we're going
+            //change directions
+            ChangeDirection();
+        }
+        else if (DoTurnAtLedges && !CheckDirection(enemy.FacingRight ? new Vector2(0.5f,-1) : new Vector2(-0.5f, -1))) {
+            //if we're supposed to turn at a ledge, and there isn't anything at our feet right in front of us
+            //change directions
+            ChangeDirection();
+        }
+
+        enemy.DesiredVelocity = new Vector2(WalkSpeed * (enemy.FacingRight ? 1 : -1), 0);
+    }
+    void ChangeDirection() {
+        if(TurnPauseTime > 0) {
+            //we're supposed to wait for TurnPauseTime before turning around, so wait
+            isPaused = true;
+            nextTurnTime = Time.time + TurnPauseTime;
+        }
+        else {
+            //just instantly change direction
+            ResolveChangeDirection();
+        }
+    }
+    void ResolveChangeDirection() {
+        isPaused = false;
+        enemy.FacingRight = !enemy.FacingRight;
+    }
+}
