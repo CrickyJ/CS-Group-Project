681da373a712a8b11db09be55884a2da6bbb92a0
Renno Sullivan <renno.hwang@gmail.com>
2018-10-14 17:31:37 -0500

    Added level 2 background
    added lv2 bground and organzied some folder, also trying to figure out how to use github

---
+++ Assets/Resources/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour {
+
+    public Transform Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Player.position.x, Player.position.y, -10);
+	}
+}
