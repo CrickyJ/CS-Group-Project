0a98940744c8aab320f0d92eb713ef58f1b3fef3
Ricky Torres <ricdot997@gmail.com>
2018-10-19 19:22:04 -0500

    Added Shooting for player (Z)
    Press z to fire a shot in the facing direction, the shot will be destroyed after 2 seconds or when colliding with the "environment" tag

---
+++ Assets/Resources/Scripts/ShotController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour {
+    private Rigidbody2D rb;
+    [SerializeField] float speed = 1, timeLimit = 2;
+
+    ShotController(string dir) //Constructor
+    {
+        Debug.Log("dir");
+    }
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+       // rb.velocity = direction * speed;
+        Destroy(this.gameObject, timeLimit); //Object will be destroyed after time limit
+	}
+
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        //Debug.Log("HIT " + hit.gameObject.tag);
+        if (hit.gameObject.CompareTag("Environment")) //If projectile collides with environement
+        {
+            //Debug.Log("Destroy");
+            Destroy(this.gameObject);
+        }
+    }
+}
