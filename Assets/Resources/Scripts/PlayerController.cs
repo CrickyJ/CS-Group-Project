b5eb4211837d685fc097f31eec763b6d6d1ade60
Ricky Torres <ricdot997@gmail.com>
2018-10-23 17:58:21 -0500

    Adjusted movement and shooting, and sprites for player direction
    Player can fire in 8 directions, depending on which arrow keys are being held. Movement is adjusted, and player sprite changes depending on direction.
    
    Project verision changed to 2018.2.12

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -19,17 +19,27 @@ public class PlayerController : PhysicsObject
     //Animation:
     private SpriteRenderer spriteRenderer;
     private Animator animator;
-    bool flipSprite = false;
+    //bool flipSprite = false;
 
     //Shooting:
     [SerializeField] float fireRate; //Time required to pass between shots
     [SerializeField] GameObject projectile; //Object / Prefab spawned for projectile
     [SerializeField] Transform shotSpawn; //Position shot will spawn from
+    private float shotSpawnDist; //default position of shotSpawn
+    private float shotSpawnDiag; //used to calculate position for diagonal shooting
+    enum facing {right=1, upright, up, upleft, left, downleft, down, downright} //possible aiming directions
+
+    //Status:
+    [SerializeField] int maxHP = 100;
+    int health;
 
     void Awake() //Initialize Player Object
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        shotSpawnDist = shotSpawn.localPosition.x;
+        shotSpawnDiag = shotSpawnDist / 1.4142f; //diagonal aiming x-y coordinates are 1/sqrt(2) of shotSpawnDist
+        health = maxHP;
     }
 
     protected override void ComputeVelocity() //Called every frame by base class: PhysicsObject
@@ -38,9 +48,10 @@ public class PlayerController : PhysicsObject
 
         Vector2 move = Vector2.zero; //Reset movement vector for input & calculations
 
-        move.x = Input.GetAxis("Horizontal");
+        //move.x = Input.GetAxis("Horizontal");
+        move.x = Input.GetAxisRaw("Horizontal");
 
-        if ((move.x > 0) == jumpedLeft && Time.time < timer) //If player is still holding right after jumping from wall on their right
+        if ((move.x > 0) == jumpedLeft && Time.time < timer) //If player is moving towards wall after jumping
         {
             //Debug.Log("INPUT REVERSED");
             move.x *= -1; //Reverse input
@@ -74,7 +85,22 @@ public class PlayerController : PhysicsObject
             }
         }
 
-        animateSprite(move);
+        if(Input.GetAxisRaw("Vertical") > 0) //If up button is held
+        {
+            if (move.x > 0.01) aim(facing.upright);
+            else if (move.x < -0.01) aim(facing.upleft);
+            else aim(facing.up);
+        }
+
+        else if (Input.GetAxisRaw("Vertical") < 0) //If down button is held
+        {
+            if (move.x > 0.01) aim(facing.downright);
+            else if (move.x < -0.01) aim(facing.downleft);
+            else if (!grounded) aim(facing.down);
+        }
+
+        else if (move.x > 0.01) aim(facing.right);
+        else if (move.x < -0.01) aim(facing.left);
 
         if (Input.GetButtonDown("Fire1"))
         {
@@ -82,23 +108,75 @@ public class PlayerController : PhysicsObject
             FireWeapon();
         }
 
-        
-
         targetVelocity = move * maxSpeed;
+    }
+
+    private void aim(facing dir) //changes sprite and shotspawn location
+    {
+        switch(dir)
+        {
+            case facing.right: //Reset to default
+                spriteRenderer.flipX = false;
+                shotSpawn.localPosition = new Vector3(shotSpawnDist, 0, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, 0);
+                animator.SetInteger("direction", 0); //resets animation to default
+                break;
+
+            case facing.upright:
+                spriteRenderer.flipX = false;
+                shotSpawn.localPosition = new Vector3(shotSpawnDiag, shotSpawnDiag, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, 45);
+                animator.SetInteger("direction", 2);
+                break;
+
+            case facing.up:
+                shotSpawn.localPosition = new Vector3(0, shotSpawnDist, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, 90);
+                animator.SetInteger("direction", 1);
+                break;
+
+            case facing.upleft:
+                spriteRenderer.flipX = true;
+                shotSpawn.localPosition = new Vector3(-shotSpawnDiag, shotSpawnDiag, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, 135);
+                animator.SetInteger("direction", 2);
+                break;
+
+            case facing.left:
+                spriteRenderer.flipX = true;
+                shotSpawn.localPosition = new Vector3(-shotSpawnDist, 0, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, 180);
+                animator.SetInteger("direction", 0); //resets animation to default
+                break;
+
+            case facing.downleft:
+                spriteRenderer.flipX = true;
+                shotSpawn.localPosition = new Vector3(-shotSpawnDiag, -shotSpawnDiag, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, -135);
+                animator.SetInteger("direction", 3);
+                break;
+
+            case facing.down:
+                shotSpawn.localPosition = new Vector3(0, -shotSpawnDist, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, -90);
+                animator.SetInteger("direction", 4);
+                break;
+
+            case facing.downright:
+                spriteRenderer.flipX = false;
+                shotSpawn.localPosition = new Vector3(shotSpawnDiag, -shotSpawnDiag, 0);
+                shotSpawn.localRotation = Quaternion.Euler(0, 0, -45);
+                animator.SetInteger("direction", 3);
+                break;
+
+            default:
+                Debug.Log("DEFAULT");
+                break;
+        }
     }
 
     private void animateSprite(Vector2 dir) //Determines direction for player sprite
     {
-        flipSprite = (spriteRenderer.flipX ? (dir.x > 0.01f) : (dir.x < -0.01f));
-        if (flipSprite)
-        {
-            
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-            shotSpawn.localPosition = new Vector3(-shotSpawn.localPosition.x, shotSpawn.localPosition.y, shotSpawn.localPosition.z);
-            //shotSpawn.Rotate(Vector3.forward * 180);
-            shotSpawn.localRotation *= Quaternion.Euler(0, 0, 180); //Flip shotspawn
-            //Debug.Log("FLIP: " + shotSpawn.rotation);
-        }
 
         //animator.SetBool("grounded", grounded);
         //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
@@ -106,10 +184,11 @@ public class PlayerController : PhysicsObject
 
     private void FireWeapon() //Determines how player will fire weapon
     {
-        //GameObject shot = GameObject.Instantiate(projectile, shotSpawn); //Instantiate shot at shotSpawn position and rotation
         Instantiate(projectile, shotSpawn.position, shotSpawn.rotation); //spawn shot -- movement handled by shotController
         //GetComponent<AudioSource>().Play(); //play audio attached to shot object
     }
+
+    //private void 
 
     protected override void WallSlide(int index) //If player is colliding with wall
     {
