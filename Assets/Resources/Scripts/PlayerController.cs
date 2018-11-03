febad8f26f19de1b9664d6470434d02dd90b8875
Ricky Torres <ricdot997@gmail.com>
2018-11-02 20:39:44 -0500

    Improved Dashing
    Dash increases speed for a small period, and has a small wait time to dash again

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ using UnityEngine;
 public class PlayerController : PhysicsObject
 {
     //Movement:
-    public float maxSpeed = 7, jumpTakeOffSpeed = 7, dashSpeed = 5;
-    //public float jumpTakeOffSpeed = 7;
+    public float maxSpeed = 7,  //Horizontal speed
+        jumpTakeOffSpeed = 7,   //Jump height
+        dashSpeed = 5,          //Speed of dash
+        dashTime = 0.5f;        //Time length of dash
+    private float nextDash = 0.0f; //Cooldown for dash
+    private bool dashing = false;   //True if player presses dash button
 
     //Wall Jumping:
     [Tooltip ("Horizontal input delay (in seconds) after wall-jumping.")]
@@ -40,17 +44,13 @@ public class PlayerController : PhysicsObject
         animator = GetComponent<Animator>();
         shotSpawnDist = shotSpawn.localPosition.x;
         shotSpawnDiag = shotSpawnDist / 1.4142f; //diagonal aiming x-y coordinates are 1/sqrt(2) of shotSpawnDist
-        //health = maxHP;
         health = 99;
     }
 
     protected override void ComputeVelocity() //Called every frame by base class: PhysicsObject
     {
         animator.SetBool("isSliding", canWallJump);
-
         Vector2 move = Vector2.zero; //Reset movement vector for input & calculations
-
-        //move.x = Input.GetAxis("Horizontal");
         move.x = Input.GetAxisRaw("Horizontal");
 
         if ((move.x > 0) == jumpedLeft && Time.time < timer) //If player is moving towards wall after jumping
@@ -165,16 +165,25 @@ public class PlayerController : PhysicsObject
             }
         }
 
-        aim(direction);
+        Aim(direction);
 
-        if(Input.GetButtonDown("Dash"))
+        if (Input.GetButtonDown("Dash") && Time.time > (nextDash + dashTime*2)) //Begin dash
+        {
+            //Debug.Log("Start Dash");
+            dashing = true; //Player speed will be increased
+            nextDash = Time.time + dashTime; //Sets time when player can next dash
+        }
+
+        if (dashing && Time.time < nextDash) //Continue dash
         {
             move.x *= dashSpeed;
+            velocity.y = 0;
+            //Debug.Log("Still dashing");
         }
+        else dashing = false; //End dash after period of time
 
         if (Input.GetButtonDown("Fire1")) //True for one frame
         {
-            //Debug.Log("FIRE");
             FireWeapon();
         }
 
@@ -183,7 +192,7 @@ public class PlayerController : PhysicsObject
 
 
 
-    private void aim(facing dir) //changes sprite and shotspawn location
+    private void Aim(facing dir) //changes sprite and shotspawn location
     {
         switch(dir)
         {
@@ -253,6 +262,8 @@ public class PlayerController : PhysicsObject
         Instantiate(projectile, shotSpawn.position, shotSpawn.rotation); //spawn shot -- movement handled by shotController
         //GetComponent<AudioSource>().Play(); //play audio attached to shot object
     }
+
+    //private float Dash(){}
 
     protected override void WallSlide(int index) //If player is colliding with wall
     {
