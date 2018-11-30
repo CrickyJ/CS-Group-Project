da48386417d8de6d56080354420f3fa911d7a1eb
Ashley <zamecnikash@gmail.com>
2018-11-29 20:49:36 -0600

    Added next level UI and game complete scene

--- Assets/Resources/Scripts/NextLevel.cs
+++ Assets/Resources/Scripts/NextLevel.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour {
+    public GameObject LevelCompleteUI;
 
     //TODO: When player collides with NextLevelTrigger, change scene to next in build
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnCollisionEnter(Collision collision)
+    {
+
+        LevelCompleteUI.SetActive(true);
+    }
+
+    public void GoNextLevel()
+    {
+        Debug.Log("Next Level!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
