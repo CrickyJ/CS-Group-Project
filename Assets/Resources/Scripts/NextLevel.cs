138643abd01d55ab66277a8981d5522f163491b6
Ricky Torres <ricdot997@gmail.com>
2018-11-29 18:53:16 -0600

    UI Objects added for each level, added NextLevelTrigger
    UI Objects added as prefabs
    NextLevelTrigger can be used to send player to next level when they reach a certain point

---
+++ Assets/Resources/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+
+    //TODO: When player collides with NextLevelTrigger, change scene to next in build
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
